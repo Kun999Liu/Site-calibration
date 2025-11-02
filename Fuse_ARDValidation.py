44bf3b5c53b0bd1ffad376375aa8ca53f0aae044
liukun <liukunjsj@163.com>
2025-11-02 19:22:58 +0800

    添加真实性检验文件

---
+++ Fuse_ARDValidation.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2025/11/2 19:22
+# @Author  : Liu Kun
+# @Email   : liukunjsj@163.com
+# @File    : Fuse_ARDValidation.py
+# @Software: PyCharm
+
+"""
+Describe:
+"""
